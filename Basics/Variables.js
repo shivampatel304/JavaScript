d4deb644935b38d3a35dd6fc47cb7070f400f339
shivampatel304 <shivampatel0304@gmail.com>
2023-05-06 19:21:07 -0400

    Adding files

---
+++ Basics/Variables.js
@@ -0,0 +1,15 @@
+var fullName = "Shivam Patel";
+
+var courseName = "React js bootcamp";
+
+var isLoggedIn = false;
+
+var loggedCount = 34;
+
+console.log(loggedCount);
+
+var paymentMode;
+
+console.log(paymentMode);
+
+paymentMode = "Credit Card";
---
+++ script.js
@@ -0,0 +1,2 @@
+console.log("Shivam Patel");
+
