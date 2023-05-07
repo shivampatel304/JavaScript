abbdd22ddddd837214440645133adf1a4126361a
shivampatel304 <shivampatel0304@gmail.com>
2023-05-07 10:01:59 -0400

    new file added

---
+++ Basics/Operators.js
@@ -0,0 +1,19 @@
+// var num1 = 7;
+// var num2 = 6;
+
+// console.log(num1*num2);
+
+// var answer = num1 > num2;
+
+// console.log(answer);
+
+var sellingPrice = 199;
+var listingPrice = 799;
+
+var discountPercent = ((listingPrice - sellingPrice)/listingPrice)*100;
+
+discountPercent = Math.round(discountPercent);
+
+console.log("Discount percentage is : " + discountPercent + "% off");
+
+ 
